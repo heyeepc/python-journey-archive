491913caeed59b85dce26415adb5d1748ffb8889
heyeepc <2675458141@qq.com>
2025-05-02 18:52:43 +0800

    Create sum() -1.py

---
+++ Python 自带的函数/sum() -1.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+print(total)  # 输出: 15
+
+data = (10, 20, 30, 40)
+total = sum(data)
+print(total)  # 输出: 100
+
+r = range(1, 6) # 表示 1 到 5 的整数序列
+total = sum(r)
+print(total)  # 输出: 15
+
+numbers = [1, 2, 3]
+start_value = 10
+total = sum(numbers, start_value)
+print(total)  # 输出: 16 (10 + 1 + 2 + 3)
