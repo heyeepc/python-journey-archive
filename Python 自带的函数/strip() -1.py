50ca587d0e7f62ebee4227de948d5a83108f894d
heyeepc <2675458141@qq.com>
2025-05-05 21:22:16 +0800

    Update strip() -1.py

--- Python 自带的函数/strip() -1.py
+++ Python 自带的函数/strip() -1.py
@@ -2,3 +2,8 @@ text3 = "---Hello---"
 stripped_text3 = text3.strip("-")
 print(f"原字符串: '{text3}'")
 print(f"处理后: '{stripped_text3}'")
+
+text4 = "ababaHelloababa"
+stripped_text4 = text4.strip("ab") # 移除 'a' 和 'b'
+print(f"原字符串: '{text4}'")
+print(f"处理后: '{stripped_text4}'")
