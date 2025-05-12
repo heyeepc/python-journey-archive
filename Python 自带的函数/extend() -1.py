aec40645c011341283552092bd1a2d3c1f5e98d0
heyeepc <2675458141@qq.com>
2025-05-12 11:47:06 +0800

    Update extend() -1.py

--- Python 自带的函数/extend() -1.py
+++ Python 自带的函数/extend() -1.py
@@ -1,3 +1,16 @@
 my_list = [1, 2, 3]
 my_list.extend([4, 5])
 print(my_list) # 输出: [1, 2, 3, 4, 5]
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+list1.extend(list2)
+print(list1)
+# 输出: [1, 2, 3, 4, 5, 6]
+# list1 被修改，list2 不变
+
+list_a = [1, 2]
+list_b = [3, 4]
+list_a.append(list_b)
+print(list_a) # 输出: [1, 2, [3, 4]]
+
