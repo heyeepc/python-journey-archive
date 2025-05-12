b5a8328396de920b4018027c7d356b03dd7cdfe7
heyeepc <2675458141@qq.com>
2025-05-12 11:37:17 +0800

    Update join() -1.py

--- Python 自带的函数/join() -1.py
+++ Python 自带的函数/join() -1.py
@@ -2,3 +2,8 @@ words = ["Hello", "World", "Python"]
 result = " ".join(words)
 print(result)
 # 输出: Hello World Python
+
+letters = ['P', 'y', 't', 'h', 'o', 'n']
+result = "".join(letters)
+print(result)
+# 输出: Python
