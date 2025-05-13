8d5599cf3821bac82bf573558535897f441387c6
heyeepc <2675458141@qq.com>
2025-05-13 13:16:08 +0800

    Update keys() -2.py

--- Python 自带的函数/keys() -2.py
+++ Python 自带的函数/keys() -2.py
@@ -3,3 +3,11 @@ my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
 keys_view = my_dict.keys()
 print(keys_view) # 输出: dict_keys(['name', 'age', 'city'])
 print(type(keys_view)) # 输出: <class 'dict_keys'>
+
+print("迭代键:")
+for key in my_dict.keys():
+    print(key)
+
+# 检查键是否存在
+print('name' in keys_view) # 输出: True
+print('address' in keys_view) # 输出: False
