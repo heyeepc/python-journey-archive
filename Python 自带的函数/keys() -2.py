080c5f8b642c6bbafbe73a4662cb80a93b7c1d54
heyeepc <2675458141@qq.com>
2025-05-13 13:09:09 +0800

    Create keys() -2.py

---
+++ Python 自带的函数/keys() -2.py
@@ -0,0 +1,5 @@
+my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+
+keys_view = my_dict.keys()
+print(keys_view) # 输出: dict_keys(['name', 'age', 'city'])
+print(type(keys_view)) # 输出: <class 'dict_keys'>
