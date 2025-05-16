3a4f47f5999b6d94848409ec31e5299c4fc7c884
heyeepc <2675458141@qq.com>
2025-05-16 14:02:49 +0800

    Update 修改属性的值 -2.py

--- 类/修改属性的值 -2.py
+++ 类/修改属性的值 -2.py
@@ -3,10 +3,18 @@ class Car:
         self.brand = brand
         self.model = model
         self.year = year
-        self.odometer = 0  # 默认里程数为 0
+        self.odometer = 0  # 默认值为 0
 
     def describe(self):
         print(f'{self.year} {self.brand} {self.model}，已行驶 {self.odometer} 公里')
 
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer:
+            self.odometer = mileage
+        else:
+            print("不能回调里程数！")
+
+
 my_car = Car("Toyota", "Camry", 2020)
+my_car.update_odometer(15000)
 my_car.describe()
