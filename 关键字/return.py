8ff65f4e56491bac35376d5aa1acb4a80f3a87b1
heyeepc <2675458141@qq.com>
2025-05-22 13:30:15 +0800

    Update return.py

--- 关键字/return.py
+++ 关键字/return.py
@@ -4,3 +4,8 @@ def multiply(x, y):
 
 result = multiply(4, 5) # 函数返回 20，该值存储在 'result' 变量中
 print(result)
+
+def my_function():
+    print("这行代码会运行。")
+    return "返回值"
+    print("这行代码不会运行。") # 这行代码是不可达的
