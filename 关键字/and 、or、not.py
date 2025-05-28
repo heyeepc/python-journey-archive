c400da91ee94ee40a809c3ef07093967c716f282
heyeepc <2675458141@qq.com>
2025-05-28 22:49:29 +0800

    Create and 、or、not.py

---
+++ 关键字/and 、or、not.py
@@ -0,0 +1,14 @@
+age = 25
+has_license = True
+
+if age >= 18 and has_license:
+    print("可以开车。")
+
+is_student = False
+is_employee = True
+if is_student or is_employee:
+    print("是学生或员工。")
+
+is_open = False
+if not is_open:
+    print("门是关着的。")
