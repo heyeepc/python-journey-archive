614fe3b88127be25be5131270161e96b47249dc9
heyeepc <2675458141@qq.com>
2025-06-13 13:58:41 +0800

    Update Exception.py

--- 异常/Exception.py
+++ 异常/Exception.py
@@ -1,3 +1,12 @@
 #Exception 是所有异常的父类
+try:
+    x = int(input("分子："))
+    y = int(input("分母："))
+    print(x / y)
+except ZeroDivisionError:
+    print("不能除以 0！")
+except ValueError:
+    print("请输入整数！")
+    
 except Exception as e:
     print("出错了：", e)
