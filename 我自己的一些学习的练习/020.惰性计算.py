cf1703a154c17c4499825067f3975d15d84d7c42
heyeepc <2675458141@qq.com>
2025-10-30 01:30:13 +0800

    Create 020.惰性计算.py

---
+++ 我自己的一些学习的练习/020.惰性计算.py
@@ -0,0 +1,17 @@
+# 程序在遇到一个表达式时，不会立刻求值，而是等到真正需要结果时才进行计算。
+
+nums = range(1, 10**9)
+print(nums)        # 不会生成十亿个数字
+print(nums[0])     # 只在这里取出需要的值
+
+
+def squares():
+    for i in range(1, 6):
+        print(f"正在计算 {i} 的平方...")
+        yield i * i  # 惰性生成
+
+gen = squares()
+for value in gen:
+    print("得到：", value)
+
+#yield 会让函数“暂停”，只有当循环取下一个值时，才继续计算。
